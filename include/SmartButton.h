fe0f9bc6206f22da20d687d40e8572b7ef57edd7
koenseeuws <168718548+involved-koen@users.noreply.github.com>
2024-05-20 19:09:12 +0200

    Properly split up SmartButton class

--- include/SmartButton.h
+++ include/SmartButton.h
@@ -5,8 +5,6 @@
 #ifndef SMARTBUTTON_H
 #define SMARTBUTTON_H
 
-#include <Arduino.h>
-
 class SmartButton {
 private:
     int pin;
@@ -26,83 +24,11 @@ private:
     bool buttonHeldDown;
 
 public:
-    explicit SmartButton(const int buttonPin) : pin(buttonPin),
-                                                //Press variables
-                                                pressLastDebouceTime(0),
-                                                pressLastButtonState(LOW),
-                                                buttonPressHandled(false),
-                                                //Release variables
-                                                releaseLastDebounceTime(0),
-                                                releaseLastButtonState(HIGH),
-                                                buttonReleaseHandled(true), //Initialized to true (because of startup)
-                                                //Hold variables
-                                                buttonDownTime(0), buttonHeldDown(false) {
-        pinMode(pin, INPUT_PULLUP);
-    }
+    explicit SmartButton(int buttonPin);
 
-    bool isPressed() {
-        const unsigned long currentTime = millis();
-        const bool buttonState = digitalRead(pin);
-
-        if (buttonState != pressLastButtonState) {
-            pressLastDebouceTime = currentTime;
-            buttonPressHandled = false; // Reset the button handled flag when state changes
-        }
-
-        if (currentTime - pressLastDebouceTime <= debounceDelay) {
-            return false;
-        }
-
-        if (buttonState == LOW && !buttonPressHandled) {
-            buttonPressHandled = true;
-            pressLastButtonState = buttonState;
-            return true;
-        }
-
-        return false;
-    }
-
-    bool isReleased() {
-        const unsigned long currentTime = millis();
-        const bool buttonState = digitalRead(pin);
-
-        if (buttonState != releaseLastButtonState) {
-            releaseLastDebounceTime = currentTime;
-            buttonReleaseHandled = false; // Reset the button handled flag when state changes
-        }
-
-        if (currentTime - releaseLastDebounceTime <= debounceDelay) {
-            return false;
-        }
-
-        if (buttonState == HIGH && !buttonReleaseHandled) {
-            buttonReleaseHandled = true;
-            releaseLastButtonState = buttonState;
-            return true;
-        }
-
-
-        return false;
-    }
-
-    bool isHeldDown() {
-        const unsigned long currentTime = millis();
-        const bool buttonState = digitalRead(pin);
-
-        if (buttonState == LOW) {
-            if (!buttonHeldDown) {
-                buttonDownTime = currentTime;
-                buttonHeldDown = true;
-            }
-            if (currentTime - buttonDownTime > holdDelay) {
-                return true;
-            }
-        } else {
-            buttonHeldDown = false;
-        }
-
-        return false;
-    }
+    bool isPressed();
+    bool isReleased();
+    bool isHeldDown();
 };
 
 #endif //SMARTBUTTON_H
--- src/SmartButton.cpp
+++ src/SmartButton.cpp
@@ -3,3 +3,83 @@
 //
 
 #include "SmartButton.h"
+#include "Arduino.h"
+
+SmartButton::SmartButton(const int buttonPin) : pin(buttonPin),
+                                                //Press variables
+                                                pressLastDebouceTime(0),
+                                                pressLastButtonState(LOW),
+                                                buttonPressHandled(false),
+                                                //Release variables
+                                                releaseLastDebounceTime(0),
+                                                releaseLastButtonState(HIGH),
+                                                buttonReleaseHandled(true), //Initialized to true (because of startup)
+                                                //Hold variables
+                                                buttonDownTime(0), buttonHeldDown(false)
+{
+    pinMode(pin, INPUT_PULLUP);
+}
+
+bool SmartButton::isPressed() {
+    const unsigned long currentTime = millis();
+    const bool buttonState = digitalRead(pin);
+
+    if (buttonState != pressLastButtonState) {
+        pressLastDebouceTime = currentTime;
+        buttonPressHandled = false; // Reset the button handled flag when state changes
+    }
+
+    if (currentTime - pressLastDebouceTime <= debounceDelay) {
+        return false;
+    }
+
+    if (buttonState == LOW && !buttonPressHandled) {
+        buttonPressHandled = true;
+        pressLastButtonState = buttonState;
+        return true;
+    }
+
+    return false;
+}
+
+bool SmartButton::isReleased() {
+    const unsigned long currentTime = millis();
+    const bool buttonState = digitalRead(pin);
+
+    if (buttonState != releaseLastButtonState) {
+        releaseLastDebounceTime = currentTime;
+        buttonReleaseHandled = false; // Reset the button handled flag when state changes
+    }
+
+    if (currentTime - releaseLastDebounceTime <= debounceDelay) {
+        return false;
+    }
+
+    if (buttonState == HIGH && !buttonReleaseHandled) {
+        buttonReleaseHandled = true;
+        releaseLastButtonState = buttonState;
+        return true;
+    }
+
+
+    return false;
+}
+
+bool SmartButton::isHeldDown() {
+    const unsigned long currentTime = millis();
+    const bool buttonState = digitalRead(pin);
+
+    if (buttonState == LOW) {
+        if (!buttonHeldDown) {
+            buttonDownTime = currentTime;
+            buttonHeldDown = true;
+        }
+        if (currentTime - buttonDownTime > holdDelay) {
+            return true;
+        }
+    } else {
+        buttonHeldDown = false;
+    }
+
+    return false;
+}
